1d0da3311b4133a19c55780e23559b52a4ba7a92
Priyankakatiyar6 <priyankakatiyar0608@gmail.com>
2024-09-10 16:43:00 +0530

    new file

---
+++ Basics/Variable.js
@@ -0,0 +1,14 @@
+const accountId = 1234;
+let accountPassword = "Pri";
+var accountEmail =" priya@gmail.com";
+accountCity = "Indore";
+
+// accountId =2;
+console.log(accountId);
+ accountPassword = "nodePri";
+ accountEmail =" priyanka@gmail.com";
+accountCity = "Ujjain";
+let accountAddress;
+
+console.table([accountEmail,accountId,accountCity,accountPassword,accountAddress]);
+
