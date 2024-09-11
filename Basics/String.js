2c281a1d7ce395ab3fd60e744c08c64a05e20574
Priyankakatiyar6 <priyankakatiyar0608@gmail.com>
2024-09-11 23:40:18 +0530

    new file

---
+++ Basics/String.js
@@ -0,0 +1,30 @@
+const Name = "Priyanka"
+const Repo = 50 ;
+
+//console.log(Name + Repo);
+ console.log( `Hello My Name is {Name} my Repo Count is {Repo} `)
+
+  const Add = new String("Hitesh-HO");
+/* console.log(Add);
+ console.log(Add.__proto__);
+ console.log(Add.length);
+ console.log(Add.toUpperCase());
+ console.log(Add.charAt(4));
+ console.log(Add.indexOf('t')); */
+ /*const newString = Add.substring(0,5);
+ console.log(newString);
+ const AnotherString = Add.slice(-8,6);
+ console.log(AnotherString);*/
+ const  Alpa = "   Priyanka    "
+ console.log(Alpa);
+ console.log(Alpa.trim());
+ const url = "http://Priyanka.com/priyanka%20katiyar"
+ console.log(url.replace('%20','_'));
+ console.log(url.includes('priyanka'));
+ console.log(url.includes('shruti'));
+ console.log(Add.split('-'));
+ 
+ 
+
+ 
+ 
