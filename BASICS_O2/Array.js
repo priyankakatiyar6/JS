bca40079ec5ce11b75c293a780f4109edd1161ce
Priyankakatiyar6 <priyankakatiyar0608@gmail.com>
2024-09-13 09:20:15 +0530

    new file

---
+++ BASICS_O2/Array.js
@@ -0,0 +1,78 @@
+// Array //
+//const Array = [1,2,3,4,5];
+/*console.log(Array[2]);
+const ALPHA = ['A','B','E','L','I'];
+console.log(ALPHA[4]);*/
+
+ //const A = new Array('Riya','Priya','Mohit','shyam','Rohan');
+ /*console.log(A[2]);
+ console.log(A);
+ A.push('Sonam');
+ console.log(A);
+ A.pop();
+ console.log(A);
+ A.unshift(9)
+ console.log(A);
+ A.shift();
+ console.log(A);  */
+
+// console.log(A.includes('Riya'));
+ //console.log(A.indexOf('Mohit'));
+
+ /*const STRING = A.join();
+ console.log(A);
+ console.log(STRING);
+ console.log(typeof STRING);
+ console.log(typeof A); */
+
+ //******* Slice And Splice **********
+
+ /*console.log("C", A);
+ 
+ const Arr = A.slice(1,3);
+ console.log(Arr);
+ console.log("E", A);
+ 
+ const Brr = A.splice(1,3);
+ console.log(Brr);
+ console.log("G", A);*/
+
+ //const number = [1,2,3,4,5]
+ //const Alpha = ['a','b','c','d','e']
+
+ /*number.push(Alpha);
+ console.log(number);
+ console.log(number[5]);
+ console.log(number[5][0]);*/
+
+// ***************** This is not good way to merge to array ***********************
+
+/*const newarray = number.concat(Alpha);
+console.log(newarray);
+console.log(newarray[7]); */
+
+/*const AnotherArray = [1,2,3,[4,5],6,7,8,[9,[0,3]]];
+const RealAArray = AnotherArray.flat(Infinity);
+console.log(RealAArray);*/
+
+console.log(Array.isArray("Priyanka"))
+console.log(Array.from("Priyanka"))
+console.log(Array.from({name:"Priyanka"}))
+
+let score1 =100;
+let score2 =200;
+let score3 =300;
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
