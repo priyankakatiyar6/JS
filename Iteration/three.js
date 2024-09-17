e73cf6daf19f5efc41335a86bcc450510f9faa84
Priyankakatiyar6 <priyankakatiyar0608@gmail.com>
2024-09-17 18:38:21 +0530

    63b93fa386404bd09d667e1689ea1c1ddf30e7eb

---
+++ Iteration/three.js
@@ -0,0 +1,33 @@
+// for of 
+//[" ","",""]
+//[{},{},{}]
+const Arr = [1,2,3,4,5]
+for(const num of Arr){
+   // console.log(num);
+}
+
+const Greeting = "HELLO WORLD!"
+for (const Greet of Greeting) {
+    //console.log(`Each Char is ${Greet}`);
+    
+}
+
+// Maps
+const map = new Map()
+map.set('IN', "India");
+map.set('USA', "United states of America")
+map.set('Fr', "France");
+map.set('IN',"India");
+//console.log(map);
+for (const [key,value] of map) {
+    console.log(key, ':-', value);
+    
+}
+const myObject = {
+    game1: 'Car',
+    game2: 'Ludo'
+}
+for (const [key,value] of myObject) {
+    console.log(key, ':-', value);
+    
+}
---
+++ Iteration/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+let index = 1
+while(index<=10){
+   // console.log(`Value of index is ${index}`);
+    index = index+2;
+}
+
+let myArray = ['Priya','mohan', 'sohan']
+let Arr =0
+while (Arr<myArray.length) {
+    //console.log(`Value is ${myArray[Arr]}`);
+    Arr = Arr +1
+    
+}
+
+//do while loop
+
+let score =11
+do{
+    console.log(`Score is ${score}`);
+    score++;
+    }while(score<=10);
